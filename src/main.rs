67f7bbc17bd5e1a24876bd0868aedeea847fee07
Thomas McNiven <hello@vevix.net>
2019-12-01 00:13:58 +1100

    refactor slightly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-#![deny(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![deny(clippy::all, clippy::pedantic, clippy::nursery)]
 
-use crate::watcher::*;
-#[cfg(windows)]
-use crate::windows::*;
-use crossbeam_channel::{select, unbounded, Receiver};
-#[cfg(windows)]
-use ctrlc::set_handler;
-use failure::Error;
-#[cfg(windows)]
-use pelite::{pattern, PeFile};
+use crate::spotify::Spotify;
+use anyhow::Result;
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
@@ -16,88 +9,48 @@ use std::sync::{
 use std::thread;
 use std::time::Duration;
 
-mod watcher;
+mod spotify;
 #[cfg(windows)]
 mod windows;
 
-fn welcome_message() {
-    println!("Spotify ads will now be muted. Enjoy your music!");
-}
+fn main() -> Result<()> {
+    let (tx, rx) = crossbeam_channel::unbounded();
+    let mut spotify = Spotify::new();
 
-fn main() -> Result<(), Error> {
-    // Watch the Spotify directory for file changes
-    let (tx, rx) = unbounded();
-    let _watcher = Watcher::watch(tx)?;
+    // Watch Spotify and start/attach to the application
+    let _watcher = spotify.watch_data_directory(tx)?;
+    spotify.run_or_attach()?;
 
-    // Continuously run and watch for interrupts to exit cleanly
+    // Keep running while waiting for a termination signal
     let running = Arc::new(AtomicBool::new(true));
-    let running_thd = running.clone();
-    set_handler(move || {
-        running_thd.store(false, Ordering::SeqCst);
+    let threaded_running = running.clone();
+    ctrlc::set_handler(move || {
+        threaded_running.store(false, Ordering::SeqCst);
     })?;
 
-    // Enjoy your music
-    mute_spotify(running, rx)
-}
+    println!("Spotify ads will now be muted. Enjoy your music!");
 
-#[cfg(windows)]
-fn mute_spotify(
-    running: Arc<AtomicBool>,
-    rx: Receiver<notify::Result<notify::Event>>,
-) -> Result<(), Error> {
-    // Find Spotify and attach to the process
-    let (process_entry, module_entry) = Windows::find_spotify()?;
-    let process = Windows::attach_to_spotify(process_entry.process_id())?;
-
-    // Read the target modules data into memory
-    let mut bytes: Vec<u8> = vec![0; module_entry.size()];
-    process.vm_read_partial(module_entry.base(), &mut bytes)?;
-
-    // Map the read data into a PeFile structure and scan for our signature address
-    let file = PeFile::from_bytes(&bytes)?;
-    let pattern = pattern!("01 00 00 00 '73 70 6F 74 69 66 79 3A");
-    let mut addresses = [0; 2];
-    file.scanner()
-        .matches(&pattern, file.headers().image_range())
-        .next(&mut addresses);
-    let target_address = (module_entry.base() + addresses[1] as usize) - 0x1400; // This needs to be fixed!
-
-    println!(
-        "Mem. Read: {:?}",
-        Windows::get_current_track(&process, target_address)?
-    );
-
-    // Get audio session control
-    let com = Windows::get_audio_session(process_entry.process_id())?;
-    welcome_message();
-
-    // Block for events from the watcher
     let mut is_muted = false;
     while running.load(Ordering::SeqCst) {
         thread::sleep(Duration::from_millis(100));
-        select! {
+        crossbeam_channel::select! {
             recv(rx) -> event => {
-                if let Ok(event) = event {
-                    if let Ok(event) = event {
-                        if Watcher::is_target_event(event) {
-                            let identifier = Windows::get_current_track(&process, target_address)?;
-                            let is_playing_ad = identifier.eq("spotify:ad");
-                            if is_playing_ad && !is_muted {
-                                is_muted = true;
-                                com.set_mute(is_playing_ad as i32)?;
-                            } else if !is_playing_ad && is_muted {
-                                // This is usually a few ms too fast and the last bit of the ad leaks through
-                                // so, artificially wait for a little bit
-                                thread::sleep(Duration::from_millis(500));
-                                is_muted = false;
-                                com.set_mute(is_playing_ad as i32)?;
-                            }
-                        }
+                let event = event??;
+                if spotify.is_valid_event(&event) {
+                    let is_playing_ad = spotify.is_playing_ad();
+                    if is_playing_ad && !is_muted {
+                        is_muted = true;
+                        spotify.set_mute(true)?;
+                    } else if !is_playing_ad && is_muted {
+                        thread::sleep(Duration::from_millis(500));
+                        is_muted = false;
+                        spotify.set_mute(false)?;
                     }
                 }
             },
             default => {},
         };
     }
+
     Ok(())
 }
---
+++ src/spotify.rs
@@ -0,0 +1,185 @@
+#[cfg(windows)]
+use crate::windows::*;
+use anyhow::{anyhow, Result};
+use directories::BaseDirs;
+#[cfg(windows)]
+use external::module::*;
+#[cfg(windows)]
+use external::process::*;
+#[cfg(windows)]
+use notify::Watcher;
+#[cfg(windows)]
+use pelite::{pattern, PeFile};
+use std::path::PathBuf;
+use std::time::Duration;
+
+type SenderType = crossbeam_channel::Sender<notify::Result<notify::Event>>;
+
+pub(crate) struct Spotify {
+    #[cfg(windows)]
+    process: Option<Process>,
+    #[cfg(windows)]
+    target_address: usize,
+    #[cfg(windows)]
+    windows_com: Windows,
+}
+
+impl Spotify {
+    pub fn new() -> Self {
+        Self {
+            #[cfg(windows)]
+            process: None,
+            #[cfg(windows)]
+            target_address: 0,
+            #[cfg(windows)]
+            windows_com: Windows::new(),
+        }
+    }
+
+    /// Watch the Spotify data directory for changes
+    pub fn watch_data_directory(&self, sender: SenderType) -> Result<notify::RecommendedWatcher> {
+        let mut watcher = notify::watcher(sender, Duration::from_millis(500))?;
+
+        // Watch each `-user` directory within the data directory This is easier than having the user specify which user
+        // is currently listening
+        let target_path = self.find_data_directory()?;
+        for entry in std::fs::read_dir(&target_path)? {
+            let entry = entry?;
+            let path = entry
+                .path()
+                .into_os_string()
+                .into_string()
+                .map_err(|err| anyhow!("{:?}", err))?;
+            if path.contains("-user") {
+                watcher.watch(path, notify::RecursiveMode::NonRecursive)?;
+            }
+        }
+
+        Ok(watcher)
+    }
+
+    #[cfg(windows)]
+    pub fn run_or_attach(&mut self) -> Result<()> {
+        // Find `spotify.exe` within the currently running processes
+        let mut processes = EnumProcess::create()?;
+        let process = match processes.find(|process| {
+            if let Ok(name) = process.exe_file().into_string() {
+                if name.to_lowercase().contains("spotify.exe") {
+                    return true;
+                }
+            }
+            false
+        }) {
+            Some(process) => process,
+            None => unreachable!(), // TODO: Start Spotify
+        };
+
+        // Find the target module within the Spotify process
+        let mut modules = EnumModules::create(process.process_id())?;
+        let module = modules
+            .find(|module| {
+                if let Ok(name) = module.name().into_string() {
+                    if name.to_lowercase().contains("chrome_elf.dll") {
+                        return true;
+                    }
+                }
+                false
+            })
+            .ok_or_else(|| anyhow!("Couldn't find target module within the Spotify process"))?;
+
+        // Attach to process
+        let rights = ProcessRights::new().vm_read().query_limited_information();
+        let process = Process::attach(process.process_id(), rights)?;
+        self.process = Some(process.clone());
+
+        // Load the module into a PeFile structure
+        let mut bytes: Vec<u8> = vec![0; module.size()];
+        process.vm_read_partial(module.base(), &mut bytes)?;
+        let file = PeFile::from_bytes(&bytes)?;
+
+        // Search for the memory pattern
+        let pattern = pattern!("01 00 00 00 '73 70 6F 74 69 66 79 3A");
+        let mut addresses = [0; 2];
+        file.scanner()
+            .matches(pattern, file.headers().image_range())
+            .next(&mut addresses);
+        self.target_address = (module.base() + addresses[1] as usize) - 0x1400;
+        println!("Memory: {:?}", self.get_current_track());
+
+        // Get an instance to the audio control owned by Spotify
+        let pid = process.pid()?;
+        self.windows_com.find_audio_control(pid)?;
+
+        Ok(())
+    }
+
+    pub fn is_playing_ad(&self) -> bool {
+        if let Ok(track) = self.get_current_track() {
+            return track.eq("spotify:ad");
+        }
+        false
+    }
+
+    #[cfg(windows)]
+    pub fn set_mute(&self, value: bool) -> Result<()> {
+        self.windows_com.set_mute(value)
+    }
+
+    pub fn is_valid_event(&self, event: &notify::Event) -> bool {
+        event.paths.iter().any(|x| {
+            if let Some(file_name) = x.file_name() {
+                return file_name == "ad-state-storage.bnk" || file_name == "recently_played.bnk";
+            }
+            false
+        })
+    }
+
+    #[cfg(windows)]
+    fn get_current_track(&self) -> Result<String> {
+        let mut data = [0; 10];
+        if let Some(process) = &self.process {
+            process.vm_read_partial(self.target_address, &mut data)?;
+        }
+        let current_track = std::str::from_utf8(&data)?;
+        Ok(current_track.to_owned())
+    }
+
+    /// Find the directory for Spotify's local data
+    fn find_data_directory(&self) -> Result<PathBuf> {
+        let base_directory =
+            BaseDirs::new().ok_or_else(|| anyhow!("Couldn't find valid home directory"))?;
+
+        // Check the default data directory path
+        // Linux    $XDG_DATA_HOME or $HOME/.local/share
+        // macOS	$HOME/Library/Application Support
+        // Windows	{FOLDERID_RoamingAppData}
+        let target_path = base_directory.data_dir().join("Spotify\\Users");
+        if target_path.as_path().exists() {
+            return Ok(target_path);
+        }
+
+        // Windows Store installs use a unique path
+        if cfg!(windows) {
+            // {FOLDERID_LocalAppData}
+            let target_path = base_directory.data_local_dir().join("Packages");
+            if target_path.as_path().exists() {
+                for entry in std::fs::read_dir(&target_path)? {
+                    let entry = entry?;
+                    let path = entry
+                        .path()
+                        .into_os_string()
+                        .into_string()
+                        .map_err(|err| anyhow!("{:?}", err))?;
+                    if path.contains("SpotifyAB.SpotifyMusic") {
+                        let target_path = entry.path().join("LocalState\\Spotify\\Users");
+                        if target_path.as_path().exists() {
+                            return Ok(target_path);
+                        }
+                    }
+                }
+            }
+        }
+
+        Err(anyhow!("Couldn't find the Spotify data directory"))
+    }
+}
